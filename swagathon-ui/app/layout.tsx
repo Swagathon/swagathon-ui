64c78e51f3591fba80c786e4f1b2c58c65244bc3
Liam <liam@chaintail.xyz>
2024-05-30 14:22:48 -0400

    updating layout

--- swagathon-ui/app/layout.tsx
+++ swagathon-ui/app/layout.tsx
@@ -1,12 +1,35 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import { Providers } from "./providers";
 
-const inter = Inter({ subsets: ["latin"] });
+
+import "@/styles/globals.css";
+import "@/styles/globalsv0.css";
+import "@/styles/fonts.css";
+import "@/styles/styles.css";
+import "@rainbow-me/rainbowkit/styles.css";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+// const open_sans = Open_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Swagathon",
+  applicationName: "Swagathon",
+
   description: "Social Ratings for Event Swag",
+  icons: "favicon.ico",
+  manifest: "site.webmanifest",
+};
+
+export const viewport: Viewport = {
+  initialScale: 1,
+  width: "device-width",
+  themeColor: [
+    { media: "(prefers-color-scheme: dark)", color: "#151b28" },
+    { media: "(prefers-color-scheme: light)", color: "#ffffff" },
+  ],
+  // userScalable: false,
+  // colorScheme: 'dark'
 };
 
 export default function RootLayout({
@@ -16,7 +39,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
