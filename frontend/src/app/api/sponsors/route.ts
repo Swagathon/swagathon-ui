74b9bb70728c518864abeb100b9882d3d11b385e
Liam <liam@chaintail.xyz>
2024-05-31 09:13:04 -0400

    add caching to API

--- frontend/src/app/api/sponsors/route.ts
+++ frontend/src/app/api/sponsors/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 import path from "path";
 import { promises as fs } from "fs";
 
+const CACHE_DURATION = 3600; // Cache duration in seconds (1 hour)
+
 export async function GET() {
   const filePath = path.join(process.cwd(), "public/data/sponsors.json");
   try {
