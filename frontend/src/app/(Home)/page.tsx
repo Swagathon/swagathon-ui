192f5de1990d52b08e678e899d54c657aada45b2
Liam <liam@chaintail.xyz>
2024-05-31 12:44:38 -0400

    add content to homepage

--- frontend/src/app/(Home)/page.tsx
+++ frontend/src/app/(Home)/page.tsx
@@ -2,17 +2,22 @@
 import { Footer } from "@/components/footer";
 import { Header } from "@/components/Header/header";
 import { Box, Flex } from "@chakra-ui/react";
+import Image from "next/image";
 
 
 export default function Home() {
   return (
     <Flex flexDirection="column" flexGrow={1}>
-
       <Box as="main" flex={1} p={4}>
-        {/* <MainPane /> */}
-        body here
+        
+        <Image
+          src="/img/swagathon-logo-full-color.png" // Adjust the path to where your image is located
+          alt="Swagathon Logo"
+          width={600}
+          height={420}
+          style={{ display: "block" }} // Ensures it behaves like a block element if needed
+        />
       </Box>
-
     </Flex>
   );
 }
